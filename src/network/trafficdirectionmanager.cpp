510efe004b8caf75117e005066feccf197c7bf14
vartsev <vartsev1@ya.ru>
2016-11-27 22:20:11 +0300

    -- Реализована инициализация на основе конфига. Создан пустой тест.

--- src/network/trafficdirectionmanager.cpp
+++ src/network/trafficdirectionmanager.cpp
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/foreach.hpp>
 
 namespace network
 {
@@ -19,52 +20,90 @@ TrafficDirectionManager::~TrafficDirectionManager()
 
 bool TrafficDirectionManager::init()
 {
-	configurationManager_.saveLoggingToFileState( true);
-	configurationManager_.saveLoggingToConsoleState( true);
-	configurationManager_.saveReconnectionInterval( 400);
+	boost::property_tree::ptree& directionList =
+		configurationManager_.getConfigurationTree().
+			get_child( configuration::ConfigurationManager::DIRECTIONS_PARAMETER_NAME);
 
-	Connection source1( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3002);
-	TrafficDirection direction1( source1);
+	std::string protocol;
+	std::string ip;
+	uint16_t port;
 
-	Connection dst( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3010);
-	Connection dst1( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3011);
-	Connection dst2( configuration::ConfigurationManager::TCP_SERVER_PARAMETER_NAME, "127.0.0.1", 3012);
-	direction1.addDistination( dst);
-	direction1.addDistination( dst1);
-	direction1.addDistination( dst2);
+	BOOST_FOREACH( boost::property_tree::ptree::const_iterator::value_type item, directionList)
+	{
+		protocol = item.second.get_child( configuration::ConfigurationManager::SRC_PARAMETER_NAME).
+				get_child( configuration::ConfigurationManager::PROTOCOL_PARAMETER_NAME).get_value<std::string>( "");
 
-	Connection source2( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3001);
-	TrafficDirection direction2( source2);
+		ip = item.second.get_child( configuration::ConfigurationManager::SRC_PARAMETER_NAME).
+				get_child( configuration::ConfigurationManager::IP_PARAMETER_NAME).get_value<std::string>( "");
 
-	Connection dst3( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3020);
-	Connection dst4( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3021);
-	Connection dst5( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3022);
-	direction2.addDistination( dst3);
-	direction2.addDistination( dst4);
-	direction2.addDistination( dst5);
+		port = boost::lexical_cast<uint16_t>( item.second.get_child( configuration::ConfigurationManager::SRC_PARAMETER_NAME).
+				get_child( configuration::ConfigurationManager::PORT_PARAMETER_NAME).get_value<std::string>( ""));
 
-	Connection source3( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3001);
-	TrafficDirection direction3( source3);
+//		std::cout << "src: " << protocol << " " << ip << " " << port << std::endl;
+		Connection source( protocol, ip, port);
+		TrafficDirection direction( source);
 
-	Connection dst6( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3030);
-	Connection dst7( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3031);
-	Connection dst8( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3032);
-	direction3.addDistination( dst6);
-	direction3.addDistination( dst7);
-	direction3.addDistination( dst5);
+		boost::property_tree::ptree& destinationTree = item.
+				second.get_child( configuration::ConfigurationManager::DST_PARAMETER_NAME);
 
-	configurationManager_.updateTrafficDirection( trafficDirectionList_);
+		BOOST_FOREACH( boost::property_tree::ptree::const_iterator::value_type subItem, destinationTree)
+		{
+			protocol = subItem.second.get_child( configuration::ConfigurationManager::PROTOCOL_PARAMETER_NAME).get_value<std::string>( "");
+			ip = subItem.second.get_child( configuration::ConfigurationManager::IP_PARAMETER_NAME).get_value<std::string>( "");
+			port = boost::lexical_cast<uint16_t>( subItem.second.get_child( configuration::ConfigurationManager::PORT_PARAMETER_NAME).get_value<std::string>( ""));
 
-	deleteTrafficDirection( direction1);
-	addTrafficDirection( direction2);
-	addTrafficDirection( direction2);
-	addTrafficDirection( direction2);
-	addTrafficDirection( direction3);
-	addTrafficDirection( direction1);
+			Connection dst( protocol, ip, port);
+			direction.addDistination( dst);
+//			std::cout << protocol << " " << ip << " " << port << std::endl;
+		}
+		addTrafficDirection( direction);
+	}
+
+
+//	configurationManager_.saveLoggingToFileState( true);
+//	configurationManager_.saveLoggingToConsoleState( true);
+//	configurationManager_.saveReconnectionInterval( 400);
+//
+//	Connection source1( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3002);
+//	TrafficDirection direction1( source1);
+//
+//	Connection dst( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3010);
+//	Connection dst1( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3011);
+//	Connection dst2( configuration::ConfigurationManager::TCP_SERVER_PARAMETER_NAME, "127.0.0.1", 3012);
+//	direction1.addDistination( dst);
+//	direction1.addDistination( dst1);
+//	direction1.addDistination( dst2);
+//
+//	Connection source2( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3001);
+//	TrafficDirection direction2( source2);
+//
+//	Connection dst3( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3020);
+//	Connection dst4( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3021);
+//	Connection dst5( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3022);
+//	direction2.addDistination( dst3);
+//	direction2.addDistination( dst4);
+//	direction2.addDistination( dst5);
+//
+//	Connection source3( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3001);
+//	TrafficDirection direction3( source3);
+//
+//	Connection dst6( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3030);
+//	Connection dst7( configuration::ConfigurationManager::TCP_CLIENT_PARAMETER_NAME, "127.0.0.1", 3031);
+//	Connection dst8( configuration::ConfigurationManager::UDP_PARAMETER_NAME, "127.0.0.1", 3032);
+//	direction3.addDistination( dst6);
+//	direction3.addDistination( dst7);
+//	direction3.addDistination( dst5);
+//
+//	deleteTrafficDirection( direction1);
+//	addTrafficDirection( direction2);
+//	addTrafficDirection( direction2);
+//	addTrafficDirection( direction2);
+//	addTrafficDirection( direction3);
+//	addTrafficDirection( direction1);
 
 //	deleteTrafficDirection( direction3);
 
-	configurationManager_.updateTrafficDirection( trafficDirectionList_);
+//	configurationManager_.updateTrafficDirection( trafficDirectionList_);
 
 	return true;
 }
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include "network/trafficdirectionmanager.h"
+#include "configuration/configurationmanager.h"
+
+int main( int argc, char* argv[])
+{
+	std::cout << "test" << std::endl;
+	return true;
+}
