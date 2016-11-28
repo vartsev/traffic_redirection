de29c71014b21ac0f669dd08e74fb292ff0297a2
eugene <vartsev1@ya.ru>
2016-11-28 17:50:29 +0300

    -- Незначительные изменения в тетсте

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,15 @@
-#include "network/trafficdirectionmanager.h"
-#include "configuration/configurationmanager.h"
+#include "network/tcpclient.h"
+#include "network/tcpserver.h"
+#include "network/udpclient.h"
+
+#include "trafficdirectionmanagertest.h"
 
 int main( int argc, char* argv[])
 {
-	std::cout << "test" << std::endl;
-	return true;
+	network::TrafficDirectionManagerTest test;
+//	network::UdpClient udpClient;
+//	network::TcpClient tcpClient;
+//	network::TcpServer tcpServer;
+
+	return 1;
 }
